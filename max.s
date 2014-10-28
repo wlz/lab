4a08a92fd515d633511f0ebcb2541214c7d61c95
Liangzhao Wu <wuliangzhao10@gmail.com>
2014-10-28 12:59:29 +0000

    add max.s .

---
+++ max.s
@@ -0,0 +1,38 @@
+/*
+int max = 0;
+int i;
+for(i = 0; i < 4; i++)
+{
+    if(data[i] > max)
+        max = data[i]
+}
+*/
+data:
+.long 6
+.long 5
+.long 10
+.long 3 
+.long 15
+.long 17
+
+.globl _start
+_start:
+
+movl $1, %edi
+movl data, %ebx
+
+op:
+movl data(, %edi, 4), %eax
+
+cmpl %eax, %ebx
+jg done
+movl %eax, %ebx
+done:
+
+addl $1, %edi
+cmpl $5, %edi 
+
+jle op 
+
+movl $1, %eax
+int $0x80
