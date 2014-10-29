1f27e581e9c674880e96e0234f1844ee1ad2fcdb
Liangzhao Wu <wuliangzhao10@gmail.com>
2014-10-29 02:55:43 +0000

    add hw.s .

---
+++ hw.s
@@ -0,0 +1,26 @@
+/*
+int main()
+{
+    printf("hello world!\n");   
+    return 0;
+}
+*/
+
+    .data
+hellostr:
+    .ascii "hello wolrd!\n" ;
+helloend:
+
+    .text
+    .globl main
+
+main:
+    movl $4, %eax
+    movl $1, %ebx
+    movl $hellostr , %ecx
+    movl $(helloend-hellostr) , %edx
+    int $0x80
+
+    movl $1, %eax
+    xorl %ebx, %ebx
+    int $0x80
