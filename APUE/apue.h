1726e4d87315f4d3475acb4a89504ac3473b2861
Liangzhao Wu <wuliangzhao10@gmail.com>
2014-12-06 16:09:12 +0800

    sync .

--- APUE/apue.h
+++ APUE/apue.h
@@ -1,13 +1,26 @@
 #ifndef _APUE_H
 #define _APUE_H
 
+#define _XOPEN_SOURCE 600
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/termios.h>
+
+#ifndef TIOCGWINSZ
+#include <sys/ioctl.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define MAXLINE 4096
+#define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+#define DIR_MODE (FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
 
 void err_quit(const char *, ...);
 void err_sys(const char *, ...);
